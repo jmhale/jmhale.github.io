80537da452e28bc0d1b953bf15ca38e62d20bd94
James Hale <james@hale.dev>
2021-09-25 13:10:47 -0400

    Update github action config and remove Travis-CI artifacts

--- _deploy/cf_purge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONEID_JAMESHALENET}/purge_cache" \
-     -H "X-Auth-Email: ${CF_EMAIL}" \
-     -H "X-Auth-Key: ${CF_API_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"purge_everything":true}'
-
-echo "Cloudflare cache purged."
